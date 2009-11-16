91ef6449d7e6556bb973b2e72377ae76012fcb8f
winton <mail@wintoni.us>
2009-11-15 22:54:50 -0800

    Spec helper fix

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,15 +4,17 @@ $:.unshift File.expand_path("#{SPEC}/../lib")
 
 require 'rubygems'
 require 'auto/require'
-require 'auto/terminal'
 require 'pp'
 
-Spec::Runner.configure do |config|
-end
+# Manually add this plugin
+Auto::Plugins.add File.expand_path("#{SPEC}/../")
 
 # For use with rspec textmate bundle
 def debug(object)
   puts "<pre>"
   puts object.pretty_inspect.gsub('<', '&lt;').gsub('>', '&gt;')
   puts "</pre>"
+end
+
+Spec::Runner.configure do |config|
 end
